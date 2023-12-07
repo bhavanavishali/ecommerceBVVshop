e71cd08e28acddf08316eb948711c77b3379c35f
bhavana <bhavana.vayshali@gmail.com>
2023-12-07 21:52:36 +0530

    paginator adding to the category page

--- djangoproject/ecommerceproject/shop/views.py
+++ djangoproject/ecommerceproject/shop/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Product,Category
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
 
 # Create your views here.
 
 
 def allProCat(request,c_slug=None):
     c_page = None
-    products = None
+    products_list = None
     if c_slug!= None:
         c_page = get_object_or_404(Category,slug=c_slug)
-        products = Product.objects.all().filter(category=c_page,available=True)
+        products_list = Product.objects.all().filter(category=c_page,available=True)
     else:
-        products = Product.objects.all().filter(available=True)
+        products_list = Product.objects.all().filter(available=True)
+    paginator = Paginator(products_list,6)
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        products = paginator.page(paginator.num_pages)
     return render(request,'category.html',{'products':products,'category':c_page})
 
 def proDetails(request,c_slug,product_slug):
