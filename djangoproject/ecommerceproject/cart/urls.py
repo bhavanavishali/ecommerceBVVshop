234f32fa5e02f71886f1ce0cf8599a038a901705
bhavana <bhavana.vayshali@gmail.com>
2023-12-11 14:01:35 +0530

    Delete the products from cart

--- djangoproject/ecommerceproject/cart/urls.py
+++ djangoproject/ecommerceproject/cart/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/<int:product_id>/',views.add_cart,name= 'add_cart'),
     path('',views.cart_details,name= 'cart_details'),
     path('remove/<int:product_id>/',views.cart_remove,name= 'cart_remove'),
+    path('full_delete/<int:product_id>/',views.full_delete,name= 'full_delete'),
 ]
